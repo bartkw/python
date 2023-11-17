e5531b50a67196c36a14ddd9e5c72fe331be3e25
Bartosz Kwiatkowski <bartosz@kwiatkowski.cf>
2023-11-17 14:00:09 +0100

    Pierwszy skrypt

--- 1.py
+++ 1.py
@@ -1,2 +1,11 @@
-print("hello world")
-print("hello world 2")
+#Pierwszy skrypt w Pythonie
+
+import sys
+#Załadowanie modułu biblioteki
+print(sys.platform)
+#podniesienie 2 do potęgi 100
+print(2 ** 100)
+
+x = 'Mielonka!'
+print (x * 8)
+#Powtórzenie łańcucha znaków
